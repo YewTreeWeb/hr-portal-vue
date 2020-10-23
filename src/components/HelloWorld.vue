e2f466328cb70e6417885c01234f206a616a6d78
Mathew Teague <mathew.teague@yewtreeweb.co.uk>
2020-10-23 23:33:05 +0100

    added custom global styles and fonts

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -28,14 +28,6 @@
       </li>
       <li>
         <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
           target="_blank"
           rel="noopener"
