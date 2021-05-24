0d5653ba9f663b02675a30cc585b60b6f60be3d9
Mathew Teague <mathew.teague@yewtreeweb.co.uk>
2021-05-24 17:05:29 +0100

    reverting TypeScript conversion

---
+++ src/components/Leave.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="columns">
+    <div
+      class="column"
+      :class="{
+        'm-r-15': index === 0,
+        'm-r-15 m-l-15': index % 2
+      }"
+      v-for="(leave, index) in filterdTypes"
+      :key="index"
+    >
+      <article class="card">
+        <h2>{{ leave.type | capitalise }} Leave</h2>
+        <ul class="leave">
+          <li>
+            <span class="leave__available" ref="leaveAvailable">{{
+              leave.available
+            }}</span>
+            days available
+          </li>
+          <li>
+            <span class="leave__approved" ref="leaveApproved">{{
+              leave.approved
+            }}</span>
+            days approved
+          </li>
+          <li>
+            <span class="leave__declined" ref="leaveDeclined">{{
+              leave.declined
+            }}</span>
+            days declined
+          </li>
+          <li>
+            <span class="leave__remaining" ref="leaveRemaining">{{
+              leave.remaining
+            }}</span>
+            days remaining
+          </li>
+        </ul>
+      </article>
+    </div>
+    <div class="column m-l-15">
+      <article class="card">
+        <h2>Sick & Medical Leave</h2>
+        <ul class="leave">
+          <li v-for="(leave, index) in companyLeave" :key="index">
+            <p v-if="leave.type !== 'birthday' && leave.type !== 'annual'">
+              {{ leave.type }}
+              <span class="leave__used" ref="leaveUsed">{{ leave.days }}</span>
+            </p>
+          </li>
+        </ul>
+      </article>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Leave",
+  props: {
+    companyLeave: Array
+  },
+  data() {
+    return {
+      title: "Company Leave Amounts"
+    };
+  },
+  computed: {
+    filterdTypes() {
+      return this.companyLeave.filter(leave => {
+        return leave.type !== "sick" && leave.type !== "medical";
+      });
+    }
+  },
+  filters: {
+    capitalise: string => {
+      const capitalFirst = string.charAt(0).toUpperCase();
+      const noCaseTail = string.slice(1, string.length);
+      return capitalFirst + noCaseTail;
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.is-center {
+  text-align: center;
+}
+.leave {
+  @include margin(20px null null);
+  > li {
+    @include margin(10px null 5px);
+    &:first-child {
+      font-weight: $weight-heavy;
+      font-size: _heading((h6, 1));
+      margin-top: 0;
+    }
+  }
+}
+</style>
---
+++ src/components/LeaveLog.vue
@@ -0,0 +1,160 @@
+<template>
+  <div class="card m-t-38">
+    <div class="container">
+      <div class="columns is-multiline" v-if="!this.error">
+        <div class="column is-12">
+          <h2>{{ title }}</h2>
+        </div>
+        <div class="column is-12" v-if="this.leaveRequests.length > 0">
+          <div
+            v-bind:id="`request-${index}`"
+            class="request section"
+            :class="{
+              'request--offset': index % 2,
+              'request--editied': editied
+            }"
+            v-for="(request, index) in this.leaveRequests"
+            :key="index"
+          >
+            <h4 class="request__date">
+              {{ formatDate(request.submitted) }}
+              <small class="request__submitted">submitted</small>
+            </h4>
+            <p
+              class="request__days"
+              ref="days"
+              @click="makeEditable"
+              contenteditable="true"
+            >
+              {{ request.days }}
+            </p>
+            <p class="request__type">{{ request.type }}</p>
+            <p
+              class="request__status"
+              ref="status"
+              @click="makeEditable"
+              contenteditable="true"
+            >
+              {{ request.status }}
+            </p>
+            <p class="request__actions">
+              <button
+                class="button is-text request__update"
+                @click="update(index)"
+                :disabled="!editied"
+              >
+                Update
+              </button>
+              <button
+                class="button is-text request__delete"
+                @click="deleteLog(index)"
+              >
+                Delete
+              </button>
+            </p>
+          </div>
+        </div>
+        <div class="column" v-else>
+          <p>There are currently no leave requests</p>
+        </div>
+      </div>
+      <p v-else>{{ this.error }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import dayjs from "dayjs";
+export default {
+  name: "LeaveLog",
+  props: {
+    leaveRequests: Array,
+    error: String
+  },
+  data() {
+    return {
+      title: "Leave",
+      days: "",
+      status: "",
+      editied: false
+    };
+  },
+  methods: {
+    formatDate(date) {
+      if (date) {
+        return dayjs(date).format("DD/MM/YYYY");
+      }
+    },
+    makeEditable(e) {
+      // add exisiting values to data properties
+      console.log({
+        days: `origin value ${this.days}`,
+        status: `origin value ${this.status}`
+      });
+
+      console.log(e.target);
+
+      if (e.target.classList.contains("request__days")) {
+        this.days = e.target.textContent;
+        console.log({ days: `saved value ${this.days}` });
+      } else {
+        this.status = e.target.textContent;
+        console.log({ days: `saved value ${this.status}` });
+      }
+
+      // check if log has class of editied if not add class and change contenteditable to true
+      if (!this.editied) {
+        this.editied = !this.editied;
+        e.target.focus();
+      }
+    },
+    update(id) {
+      // if value has changed from stored data value, remove class editied
+      // push updated data to parent component
+      this.$emit("update", {
+        id,
+        days: this.$refs.days[0].textContent.trim(),
+        status: this.$refs.status[0].textContent.trim()
+      });
+      this.editied = false;
+    },
+    deleteLog(id) {
+      this.$emit("delete", { id });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  min-height: 539px;
+}
+.request {
+  @include flex(center, space-between, row);
+  border-bottom: 1px solid v(colour-grey-dark);
+  &:last-of-type {
+    border-bottom: none;
+  }
+  &__submitted {
+    display: block;
+    color: v(colour-grey-light);
+  }
+  &__days,
+  &__status {
+    @include padding(null 15px);
+    &:hover {
+      cursor: pointer;
+    }
+  }
+  &__actions {
+    button {
+      &:first-of-type {
+        border-left: v(colour-grey-dark);
+      }
+    }
+  }
+}
+.button.is-text {
+  text-decoration: none !important;
+}
+</style>
